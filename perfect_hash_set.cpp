793e05bcedb174ad5ec74e32949bf806879f4cc9
Nikolay Zinov <nzinov@gmail.com>
2014-11-24 10:17:32 +0300

    next iteration

--- perfect_hash_set.cpp
+++ perfect_hash_set.cpp
@@ -1,10 +1,11 @@
 #include <vector>
-#include <cstdlib>
+#include <random>
 
 using std::vector;
+using std::size_t;
 
-typedef long long int i64;
-typedef unsigned int size_t;
+typedef unsigned int ui32;
+typedef unsigned long long ui64;
 
 struct KeyCollision {
 };
@@ -28,30 +29,30 @@ size_t get_prime_above(size_t m) {
 }
 
 struct UniversalHashFunction {
-    size_t a, b, m, p;
+    size_t a, b, m;
+    const ui64 P = (1 << 32) + 15;
 
-    void init(size_t m, size_t p) {
+    void init(size_t m) {
         this->m = m;
-        this->p = p;
     }
 
     void randomize() {
-        a = rand();
-        b = rand();
+        a = rand() % p + 1;
+        b = rand() % p;
     }
 
-    size_t operator() (i64 x) const {
-        return (a * (unsigned long long)x + b) % p % m;
+    size_t operator() (ui32 x) const {
+        return (a * x + b) % p % m;
     }
 };
 
 struct SecondLevelTable {
     UniversalHashFunction hash;
-    vector<i64> keys;
+    vector<ui32> keys;
     vector<bool> table;
-    vector<i64> map;
+    vector<ui32> map;
 
-    bool get_iter(i64 key, vector<bool>::iterator& iter) {
+    bool get_iter(ui32 key, vector<bool>::iterator& iter) {
         if (keys.empty()) {
             return false;
         }
@@ -63,7 +64,7 @@ struct SecondLevelTable {
         return true;
     }
 
-    void add(i64 key) {
+    void add(ui32 key) {
         keys.push_back(key);
     }
 
@@ -73,8 +74,7 @@ struct SecondLevelTable {
             return;
         }
         size_t m = n*n;
-        size_t p = get_prime_above(m);
-        hash.init(m, p);
+        hash.init(m);
         table.resize(m);
         map.resize(m);
         bool fit = false;
@@ -93,44 +93,52 @@ struct SecondLevelTable {
                 table[index] = true;
                 map[index] = keys[i];
             }
-            for (size_t i = 0; i < m; i++) {
-                table[i] = false;
-            }
+            std::fill(table.begin(), table.end(), false);
         }
+    }
+
+    size_t size() const {
+        return table.size();
     }
 };
 
-class HashSet {
+class PerfectHashSet {
     UniversalHashFunction hash;
-    vector<i64> keys;
+    vector<ui32> keys;
     vector<SecondLevelTable> table;
 
-    bool get_iter(i64 key, vector<bool>::iterator& iter ) {
-        return table[hash(key)].get_iter(key, iter);           
+    bool get_iter(ui32 key, vector<bool>::iterator& iter ) {
+        return table[hash(key)].get_iter(key, iter);
     }
 
     public:
-    void init(const vector<i64>& keys) {
+    void init(const vector<ui32>& keys) {
         this->keys = keys;
         size_t m = keys.size();
-        size_t p = get_prime_above(m);
-        hash.init(m, p);
-        hash.randomize();
         table.resize(m);
-        for (size_t i = 0; i < m; ++i) { 
-            table[hash(keys[i])].add(keys[i]);
-        }
-        for (size_t i = 0; i < m; ++i) {
-            table[i].init();
+        hash.init(m);
+        while (true) {
+            hash.randomize();
+            for (ui32 key : keys) {
+                table[hash(key)].add(key);
+            }
+            size_t total_size = 0;
+            for (size_t i = 0; i < m; ++i) {
+                table[i].init();
+                total_size += table[i].size();
+            }
+            if (total_size <= 4*m) {
+                break;
+            }
         }
     }
 
-    bool has(i64 key) {
+    bool has(ui32 key) {
         vector<bool>::iterator iter;
         return get_iter(key, iter) && *iter;
     }
 
-    void set(i64 key, bool value = true) {
+    void insert(ui32 key, bool value = true) {
         vector<bool>::iterator iter;
         if (!get_iter(key, iter)) {
             throw KeyCollision();
@@ -138,8 +146,7 @@ class HashSet {
         *iter = value;
     }
 
-    void erase(i64 key) {
-        set(key, false);
+    void erase(ui32 key) {
+        insert(key, false);
     }
 };
-
--- task2.cpp
+++ task2.cpp
@@ -1,15 +1,52 @@
 #include <vector>
 #include <cassert>
+#include <utility>
 #include "perfect_hash_set.cpp"
 
-int main() {
-    HashSet set;
-    const vector<i64> data = {1, 2, 3, 4, 5, 55566};
+void test_interface() {
+    PerfectHashSet set;
+    vector<ui32> data = {1, 2, 3, 4, 5, 55566};
     set.init(data);
     assert(set.has(451) == false);
     assert(set.has(2) == false);
-    set.set(2);
+    set.insert(2);
     assert(set.has(2) == true);
     set.erase(2);
     assert(set.has(2) == false);
+}
+
+void test_duplicated_keys() {
+    PerfectHashSet set;
+    vector<ui32> data = {1, 1};
+    set.init(data);
+}
+
+void test_big_data() {
+}
+
+void test_random() {
+}
+
+void test_collision() {
+    PerfectHashSet set;
+    vector<ui32> data = {1};
+    set.init(data);
+    set.insert(2);
+}
+
+template <typename Ex, typename Func> void assert_throws(Func f) {
+    bool catched = false;
+    try {
+        f();
+    }
+    catch (Ex) {
+        catched = true;
+    }
+    assert(catched);
+}
+
+int main() {
+    test_interface();
+    assert_throws<DuplicateKeys>(test_duplicated_keys);
+    assert_throws<KeyCollision>(test_collision);
 }

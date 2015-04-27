8bab32439059a88dec7d4762e51157ad1eb019d4
Nikolay Zinov <nzinov@gmail.com>
2015-04-27 22:23:23 +0300

    rename variables

--- semester2/task3/solution.cpp
+++ semester2/task3/solution.cpp
@@ -7,73 +7,73 @@ const size_t INF = 1000000000;
 
 vector<vector<size_t> > solution(string a, string b) {
     size_t n = max(a.length(), b.length()) + 1;
-    vector<vector<size_t> > ih(n, vector<size_t>(n));
-    vector<vector<size_t> > iv(n, vector<size_t>(n));
-    vector<size_t> VG(n);
-    vector<size_t> DG0(n);
+    vector<vector<size_t> > horizontal_weight(n, vector<size_t>(n));
+    vector<vector<size_t> > vertical_weight(n, vector<size_t>(n));
+    vector<size_t> unique_elements(n);
+    vector<size_t> first_line(n);
     for (size_t j = 0; j <= b.length(); ++j) {
-        ih[0][j] = j;
+        horizontal_weight[0][j] = j;
     }
     for (size_t l = 0; l <= a.length(); ++l) {
-        iv[l][0] = 0;
+        vertical_weight[l][0] = 0;
     }
     for (size_t l = 1; l <= a.length(); ++l) {
         for (size_t j = 1; j <= b.length(); ++j) {
             if (a[l - 1] != b[j - 1]) {
-                ih[l][j] = max(iv[l][j-1], ih[l-1][j]);
-                iv[l][j] = min(iv[l][j-1], ih[l-1][j]);
+                horizontal_weight[l][j] = max(vertical_weight[l][j-1], horizontal_weight[l-1][j]);
+                vertical_weight[l][j] = min(vertical_weight[l][j-1], horizontal_weight[l-1][j]);
             } else {
-                ih[l][j] = iv[l][j-1];
-                iv[l][j] = ih[l-1][j];
+                horizontal_weight[l][j] = vertical_weight[l][j-1];
+                vertical_weight[l][j] = horizontal_weight[l-1][j];
             }
         }
     }
     for (size_t j = 1; j <= b.length(); ++j) {
-        VG[j] = INF;
+        unique_elements[j] = INF;
     }
-    DG0[0] = 0;
+    first_line[0] = 0;
     size_t i = 1;
     for (size_t j = 1; j <= b.length(); ++j) {
-        if (ih[a.length()][j] == 0) {
-            DG0[i] = j;
+        if (horizontal_weight[a.length()][j] == 0) {
+            first_line[i] = j;
             ++i;
         } else {
-            VG[ih[a.length()][j]] = j;
+            unique_elements[horizontal_weight[a.length()][j]] = j;
         }
     }
     for (size_t l = i; l <= a.length(); ++l) {
-        DG0[l] = INF;
+        first_line[l] = INF;
     }
-    vector<vector<size_t> > D(n, vector<size_t>(n));
-    std::copy(DG0.begin(), DG0.end(), D[0].begin());
+    vector<vector<size_t> > graph_points(n, vector<size_t>(n));
+    std::copy(first_line.begin(), first_line.end(), graph_points[0].begin());
     for (size_t i = 1; i <= b.length(); ++i) {
         size_t k = 1;
         for (size_t j = 0; j <= a.length(); ++j) {
-            size_t cur = (k > a.length() ? INF : D[i - 1][k]);
-            if (VG[i] < cur) {
-                D[i][j] = VG[i];
-                VG[i] = INF;
+            size_t cur = (k > a.length() ? INF : graph_points[i - 1][k]);
+            if (unique_elements[i] < cur) {
+                graph_points[i][j] = unique_elements[i];
+                unique_elements[i] = INF;
             } else {
-                D[i][j] = cur;
+                graph_points[i][j] = cur;
                 ++k;
             }
         }
     }
-    std::vector <std::vector < size_t > > C(b.length() + 1, vector<size_t>(b.length() + 1, 0));
+    std::vector <std::vector < size_t > > answer(b.length() + 1, vector<size_t>(b.length() + 1, 0));
     for (size_t i = 0; i <= b.length(); ++i) {
         for (size_t j = 0; j <= a.length(); ++j) {
-            if (D[i][j] != INF) {
-                C[i][D[i][j]] = j;
-                if (D[i][j] > 0 && j > 0) {
-                    C[i][D[i][j] - 1] = j - 1;
+            if (graph_points[i][j] != INF) {
+                answer[i][graph_points[i][j]] = j;
+                if (graph_points[i][j] > 0 && j > 0) {
+                    answer[i][graph_points[i][j] - 1] = j - 1;
                 }
             }
         }
     }
     for (size_t i = 0; i <= b.length(); ++i) {
         for (size_t j = 1; j <= b.length(); ++j) {
-            C[i][j] = max(C[i][j], C[i][j-1]);
+            answer[i][j] = max(answer[i][j], answer[i][j-1]);
         }
     }
-    return C;
+    return answer;
 }
--- semester2/task3/stupid_solution.cpp
+++ semester2/task3/stupid_solution.cpp
@@ -9,17 +9,17 @@ vector<vector<size_t> > stupid_solution(string a, string b) {
     size_t n = a.length();
     size_t m = b.length();
     vector<vector<size_t> > ans(m+1, vector<size_t>(m+1, 0));
-    for (size_t l = 0; l < m; ++l) {
-        vector<vector<size_t> > cur(m+1, vector<size_t>(n+1));
-        for (size_t r = 1; r < m - l + 1; ++r) {
-            for (size_t x = 1; x < n + 1; ++x) {
-               if (b[l + r - 1] == a[x - 1]) {
-                   cur[r][x] = cur[r-1][x-1] + 1;
+    for (size_t left_border = 0; left_border < m; ++left_border) {
+        vector<vector<size_t> > current_table(m+1, vector<size_t>(n+1));
+        for (size_t right_border = 1; right_border < m - left_border + 1; ++right_border) {
+            for (size_t position = 1; position < n + 1; ++position) {
+               if (b[left_border + right_border - 1] == a[position - 1]) {
+                   current_table[right_border][position] = current_table[right_border-1][position-1] + 1;
                } else {
-                   cur[r][x] = std::max(cur[r-1][x], cur[r][x-1]);
+                   current_table[right_border][position] = std::max(current_table[right_border-1][position], current_table[right_border][position-1]);
                }
             }
-            ans[l][l + r] = cur[r][n];
+            ans[left_border][left_border + right_border] = current_table[right_border][n];
         }
     }
     return ans;

faa137168da6067286bfa445564994925f96dfb5
Nikolay Zinov <nzinov@gmail.com>
2015-09-30 16:56:09 +0300

    realize new data structure

--- semester3/maxflow/graph.h
+++ semester3/maxflow/graph.h
@@ -1,8 +1,10 @@
+#include <ctime>
 #include <vector>
 #include <list>
 #include <cassert>
 #include <algorithm>
 
+typedef unsigned long size_t;
 using std::vector;
 using std::list;
 
@@ -13,10 +15,9 @@ struct Edge;
 struct Vertex {
     unsigned excess;
     unsigned height;
-    size_t id;
-    vector<Edge>::iterator current_edge;
+    size_t current_neighbour;
     
-    Vertex(size_t id, vector<Edge>& edges) : id(id), current_edge(edges[id].begin()) {};
+    Vertex() : excess(0), height(0), current_neighbour(0) {};
 };
 
 struct Edge {
@@ -33,58 +34,71 @@ struct Edge {
     bool is_open() {
         return extra_capacity() > 0;
     }
+
+    void run_flow(int value) {
+        flow += value;
+    }
 };
 
 class Graph {
-    list<Vertex> vertices;
+    vector<Vertex> vertices;
     vector<vector<Edge>> edges;
     list<Vertex>::iterator current_vertex;
 
-    Edge& get_edge(Vertex* tail, Vertex* head) {
-        return edges[tail->id][head->id];
+    Graph(size_t vertex_number)
+        : vertices(vertex_number), edges(vertex_number, vector<Edge>(vertex_number)) {
+        vertices.front().height = vertex_number;
     }
 
-    void add_edge(Vertex* tail, Vertex* head, unsigned capacity) {
-        get_edge(tail, head).capacity = capacity;
+    inline bool is_out(size_t vertex) {
+        return vertex == vertices.size() - 1;
     }
 
-    void add_vertex() {
-        vertices.emplace_back(vertices.size(), vertices);
+    void run_flow(size_t from, size_t to, int value) {
+        edges[from][to].run_flow(value);
+        edges[to][from].run_flow(-value);
     }
 
-    void push(Vertex* source, Vertex* target) {
-        Edge& edge = get_edge(source, target);
-        if (edge.extra_capacity() == 0 || source->height > target->height) { 
+    void add_edge(size_t tail, size_t head, unsigned capacity) {
+        edges[tail][head].capacity = capacity;
+        if (is_out(head)) {
+            edges[tail][head].run_flow(capacity);
+        } 
+    }
+
+    void push(size_t source, size_t target) {
+        Edge& edge = edges[source][target];
+        if (edge.extra_capacity() == 0 || vertices[source].height > vertices[target].height) { 
             return;
         }
-        assert(source->excess > 0);
-        assert(source->height == target->height + 1);
+        assert(vertices[source].excess > 0);
+        assert(vertices[source].height == vertices[target].height + 1);
         assert(edge.extra_capacity() > 0);
-        unsigned extra_flow = std::max(source->excess, edge.extra_capacity());
-        source->excess -= extra_flow;
-        edge.flow += extra_flow;
+        unsigned extra_flow = std::max(vertices[source].excess, edge.extra_capacity());
+        vertices[source].excess -= extra_flow;
+        edge.run_flow(extra_flow);
     }
 
-    void relabel(Vertex* vertex) {
-        assert(vertex->excess > 0);
+    void relabel(size_t vertex) {
+        assert(vertices[vertex].excess > 0);
         unsigned lowest_height = INF;
-        for (Vertex& adjacent : vertices) {
-            if (!get_edge(vertex, &adjacent).is_open()) {
-                continue;
+        for (size_t neighbour = 0; neighbour < vertices.size(); ++neighbour) {
+            if (edges[vertex][neighbour].is_open()) {
+                lowest_height = std::min(lowest_height, vertices[neighbour].height);
             }
-            lowest_height = std::min(lowest_height, adjacent.height);
         }
-        assert(lowest_height >= vertex->height);
-        vertex->height = lowest_height + 1;
+        assert(lowest_height >= vertices[vertex].height);
+        vertices[vertex].height = lowest_height + 1;
     }
 
-    void discharge(Vertex* vertex) {
-        while (vertex->excess > 0) {
-            if (vertex->current_edge == edges[vertex->id].end()) { 
+    void discharge(size_t vertex) {
+        while (vertices[vertex].excess > 0) {
+            if (vertices[vertex].current_neighbour == vertices.size()) { 
                 relabel(vertex);
-                vertex->current_edge = edges[vertex->id].begin();
+                vertices[vertex].current_neighbour = 0;
             }
-            push(vertex, &(vertex->current_edge->head));
+            push(vertex, vertices[vertex].current_neighbour);
+            ++vertices[vertex].current_neighbour;
         };
     }
 

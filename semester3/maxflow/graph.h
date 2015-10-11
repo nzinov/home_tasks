38f2fbb4099de2def4452e1caaf7b05a14d441fd
Nikolay Zinov <nzinov@gmail.com>
2015-10-11 20:22:32 +0300

    incorporate changes from ejudge.cpp

--- semester3/maxflow/graph.h
+++ semester3/maxflow/graph.h
@@ -1,13 +1,12 @@
 #include <ctime>
 #include <vector>
-#include <list>
 #include <cassert>
 #include <algorithm>
-#include <iostream>
+#include <queue>
 
 typedef unsigned long size_t;
 using std::vector;
-using std::list;
+using std::queue;
 
 long long INF = 1000000;
 
@@ -16,8 +15,10 @@ struct Edge;
 struct Vertex {
     long long excess;
     long long height;
+    size_t current;
+    vector<size_t> neighbours;
     
-    Vertex() : excess(0), height(0) {};
+    Vertex() : excess(0), height(0), current(0) {};
 };
 
 struct Edge {
@@ -32,31 +33,44 @@ struct Edge {
     }
 
     inline bool is_open() {
-        return extra_capacity() > 0;
-    }
-
-    inline void run_flow(long long value) {
-        flow += value;
-        assert(flow <= capacity);
+        return capacity > flow;
     }
 };
 
 class Network {
     vector<Vertex> vertices;
     vector<vector<Edge> > edges;
-    list<Vertex>::iterator current_vertex;
+    queue<size_t> q;
+    vector<size_t> heights;
 
-    void run_flow(size_t from, size_t to, long long value) {
-        edges[from][to].run_flow(value);
-        edges[to][from].run_flow(-value);
-        vertices[from].excess -= value;
-        vertices[to].excess += value;
+    inline void set_height(size_t vertex, long long height) {
+        long long prev_height = vertices[vertex].height;
+        vertices[vertex].height = height;
+        heights[prev_height]--;
+        heights[height]++;
+        if (heights[prev_height] == 0) {
+            for (size_t v = 0; v < vertices.size(); ++v) {
+                if (vertices[v].height > prev_height) {
+                    vertices[v].height = vertices.size();
+                }
+            }
+        }
     }
 
-    void push(size_t source, size_t target) {
-        std::cerr << "push(" << source << ", " << target << ")" << std::endl;
+    inline void run_flow(size_t from, size_t to, long long value) {
+        edges[from][to].flow += value;
+        edges[to][from].flow -= value;
+        vertices[from].excess -= value;
+        bool flag = vertices[to].excess > 0;
+        vertices[to].excess += value;
+        if (!flag && to != 0 && to != vertices.size() - 1 && vertices[to].excess > 0) { 
+            q.push(to);
+        }
+    }
+
+    inline void push(size_t source, size_t target) {
         Edge& edge = edges[source][target];
-        if (edge.extra_capacity() == 0 || vertices[source].height != vertices[target].height + 1) { 
+        if (!edge.is_open() || vertices[source].height != vertices[target].height + 1) { 
             return;
         }
         assert(vertices[source].excess > 0);
@@ -66,67 +80,50 @@ class Network {
         run_flow(source, target, extra_flow);
     }
 
-    void relabel(size_t vertex) {
-        std::cerr << "relabel(" << vertex << ")" << std::endl;
-        assert(vertices[vertex].excess > 0);
+    inline void relabel(size_t vertex) {
         long long lowest_height = INF;
-        for (size_t neighbour = 0; neighbour < vertices.size(); ++neighbour) {
-            if (edges[vertex][neighbour].is_open()) {
-                lowest_height = std::min(lowest_height, vertices[neighbour].height);
+        for (size_t i = 0; i < vertices[vertex].neighbours.size(); ++i) {
+            if (edges[vertex][vertices[vertex].neighbours[i]].is_open()) {
+                lowest_height = std::min(lowest_height, vertices[vertices[vertex].neighbours[i]].height);
             }
         }
-        assert(lowest_height >= vertices[vertex].height);
-        vertices[vertex].height = lowest_height + 1;
+        set_height(vertex, lowest_height + 1);
     }
 
-    bool discharge(size_t vertex) {
-        std::cerr << "discharge(" << vertex << ")" << std::endl;
+    inline bool discharge(size_t vertex) {
         bool updated = false;
         size_t current_neighbour = 0;
         while (vertices[vertex].excess > 0) {
             updated = true;
-            if (current_neighbour == vertices.size()) { 
+            if (vertices[vertex].current == vertices.size()) { 
                 relabel(vertex);
-                current_neighbour = 0;
+                vertices[vertex].current = 0;
             }
-            push(vertex, current_neighbour);
-            ++current_neighbour;
+            push(vertex, vertices[vertex].current);
+            vertices[vertex].current++;
         };
         return updated;
     }
 
     void generate_flow() {
-        bool found = false;
-        while (!found) {
-            found = true;
-            for (size_t vertex = 1; vertex < vertices.size() - 1; ++vertex) {
-                if (discharge(vertex)) {
-                    found = false;
-                }
-            }
-        }
-        for (size_t i = 0; i < vertices.size(); ++i) {
-            for (size_t j = 0; j < vertices.size(); ++j) {
-                std::cerr << edges[i][j].capacity << ' ';
-            }
-            std::cerr << std::endl;
-        }
-        for (size_t i = 0; i < vertices.size(); ++i) {
-            for (size_t j = 0; j < vertices.size(); ++j) {
-                std::cerr << edges[i][j].flow << ' ';
-            }
-            std::cerr << std::endl;
+        while (!q.empty()) {
+            discharge(q.front());
+            q.pop();
         }
     }
 
 public:
     Network(size_t vertex_number)
-        : vertices(vertex_number), edges(vertex_number, vector<Edge>(vertex_number)) {
+        : vertices(vertex_number), edges(vertex_number, vector<Edge>(vertex_number)), heights(2000) {
             vertices.front().height = vertex_number;
+            heights[0] = vertex_number - 1;
+            heights[vertex_number] = 1;
     }
 
     Edge* add_edge(size_t tail, size_t head, unsigned capacity) {
-        edges[tail][head].capacity = capacity;
+        edges[tail][head].capacity += capacity;
+        vertices[tail].neighbours.push_back(head);
+        vertices[head].neighbours.push_back(tail);
         if (tail == 0) {
             run_flow(tail, head, capacity);
         } 

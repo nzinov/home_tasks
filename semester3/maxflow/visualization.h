3ebad6dc589c0cebcf5ee493ccc4bf458cbd0c1e
Nikolay Zinov <nzinov@gmail.com>
2015-11-03 09:43:13 +0300

    add visualization.h

---
+++ semester3/maxflow/visualization.h
@@ -0,0 +1,80 @@
+#ifndef VISUALIZATION
+#define VISUALIZATION
+#include <QRectF>
+#include <QBrush>
+#include <QPainter>
+#include <QFont>
+#include <QVector2D>
+#include <Qt>
+#include <QWidget>
+#include "graph.h"
+#include "log.h"
+
+const int VERTEX_RADIUS = 10;
+const int HEIGHT_MULTIPLIER = 1;
+
+class ShadowedGraphicsElement {
+public:
+    virtual void paint(QPainter* painter) = 0;
+    virtual void paintShadow(QPainter* painter) = 0;
+    virtual void updateNetwork(const Network& network) = 0;
+};
+
+class VisualVertex : public ShadowedGraphicsElement {
+public:
+    QPoint position;
+    size_t id;
+    const Vertex* vertex;
+    std::vector<VisualVertex*> adjacent;
+
+public:
+    VisualVertex(size_t id); 
+    QPoint getPosition(); 
+    QPoint getShadowPosition(); 
+    int height(); 
+    virtual void updateNetwork(const Network& network); 
+    virtual void paint(QPainter* painter); 
+    virtual void paintShadow(QPainter* painter); 
+    void addNeighbour(VisualVertex* vertex); 
+    QVector2D calculateSprings(); 
+    QVector2D calculateCharge(const std::vector<VisualVertex>& vertices); 
+    void simulate(const std::vector<VisualVertex>& vertices); 
+};
+
+const int ARROW_LENGTH = 20;
+const int ARROW_WIDTH = 5;
+const int LABEL_SHIFT = 20;
+const int SHIFT = 50;
+const int FLOW_FACTOR = 3;
+
+class VisualEdge : public ShadowedGraphicsElement {
+public:
+    VisualVertex* tail;
+    VisualVertex* head;
+    const Edge* edge;
+
+public:
+    VisualEdge(VisualVertex* tail, VisualVertex* head); 
+    virtual void updateNetwork(const Network& network); 
+    virtual void paint(QPainter* painter); 
+    virtual void paintShadow(QPainter* painter); 
+};
+
+class Visualization : public QWidget {
+    Q_OBJECT;
+    std::vector<VisualVertex> vertices;
+    std::vector<VisualEdge> edges;
+    Log log;
+    Network network;
+
+    public:
+    void load(Network network); 
+    virtual void paintEvent(QPaintEvent*); 
+    void updateNetwork(const Network& network); 
+    VisualVertex* addVertex(); 
+    VisualEdge* addEdge(size_t tail, size_t head); 
+
+public slots:
+    void simulate();
+};
+#endif

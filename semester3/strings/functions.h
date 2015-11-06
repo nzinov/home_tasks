b3949f944bcabe5be0d38ce5ef9aba3b6372f9f4
Nikolay Zinov <nzinov@gmail.com>
2015-11-06 17:15:26 +0300

    refactor to separate functions

--- semester3/strings/functions.h
+++ semester3/strings/functions.h
@@ -103,20 +103,3 @@ string decode_prefix(vector<int> data) {
     }
     return s;
 }
-int main() {
-    vector<int> data;
-    while (!std::cin.eof()) {
-        int current;
-        std::cin >> current;
-        data.push_back(current);
-    }
-    data.pop_back();
-    string ans = decode_prefix(data);
-    if (ans.length() == 5) {
-        for (int i = 0; i < ans.length(); ++i) {
-            std::cout << ans[i] << ' ';
-        }
-    } else {
-        std::cout << ans;
-    }
-}

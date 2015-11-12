8a58d94abea028c3d21a37548d1ba09c7ffb96a9
Nikolay Zinov <nzinov@gmail.com>
2015-11-13 00:49:47 +0300

    remove hacks

--- semester3/strings/c1.cpp
+++ semester3/strings/c1.cpp
@@ -2,18 +2,10 @@
 
 int main() {
     vector<int> data;
-    while (!std::cin.eof()) {
-        int current;
-        std::cin >> current;
+    int current;
+    while (std::cin >> current) {
         data.push_back(current);
     }
-    //data.pop_back();
     string ans = decode_prefix(data);
-    if (ans.length() == 5) {
-        for (int i = 0; i < ans.length(); ++i) {
-            std::cout << ans[i] << ' ';
-        }
-    } else {
-        std::cout << ans;
-    }
+    std::cout << ans;
 }
--- semester3/strings/c2.cpp
+++ semester3/strings/c2.cpp
@@ -2,18 +2,10 @@
 
 int main() {
     vector<int> data;
-    while (!std::cin.eof()) {
-        int current;
-        std::cin >> current;
+    int current;
+    while (std::cin >> current) {
         data.push_back(current);
     }
-    //data.pop_back();
     string ans = decode_prefix(z_to_prefix(data));
-    if (ans.length() == 6) {
-        for (int i = 0; i < ans.length() - 1; ++i) {
-            std::cout << ans[i] << ' ';
-        }
-    } else {
-        std::cout << ans;
-    }
+    std::cout << ans;
 }

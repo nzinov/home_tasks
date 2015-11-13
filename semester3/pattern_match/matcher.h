1bc93ed86b2391431c6d45d60c8bd1bea454e8b9
Nikolay Zinov <nzinov@gmail.com>
2015-11-14 00:36:05 +0300

    fix that stupid bug which ruined an evening

--- semester3/pattern_match/matcher.h
+++ semester3/pattern_match/matcher.h
@@ -12,7 +12,9 @@ public:
     Matcher(const std::string& pattern);
     ~Matcher() {};
     void process_occurence(int position, int block_id) {
-        counts[position - offsets[block_id]]++;
+        if (position - offsets[block_id] >= 0) {
+            counts[position - offsets[block_id]]++;
+        }
     }
         
     void find_matches(const std::string& text, void (*callback)(int position));

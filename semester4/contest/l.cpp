4507645fc2fa0b51f0464844492f5f4b83229c75
Nikolay Zinov <nzinov@gmail.com>
2016-04-01 15:33:39 +0300

    L that RT`s on contest

--- semester4/contest/l.cpp
+++ semester4/contest/l.cpp
@@ -1,10 +1,8 @@
 #include <iomanip>
 #include <fstream>
 #include <iostream>
-#include <cmath>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 #ifdef DEBUG
 #define debug(s) std::cerr << s << std::endl
@@ -12,21 +10,48 @@
 #define debug(s)
 #endif
 
-decltype(new char[10000][10000]) tree;
+int n;
+int tree[100000];
+int numbers[100000];
 
-int sum(int x, int y) {
-    if (x == -1 || y == -1) {
+struct Margin {
+    int left;
+    int right;
+};
+
+void add(int x, int a) {
+    for (int i = x; i < n; i = (i | (i + 1))) {
+        tree[i] += a;
+    }
+}
+
+int sum(int x) {
+    if (x == -1) {
         return 0;
     }
     int result = 0;
     for (int i = x; i >= 0; i = (i & (i + 1)) - 1) {
-        for (int j = y; j >= 0; j = (j & (j + 1)) - 1) {
-            result += tree[i][j];
-        }
+        result += tree[i];
     }
-    debug(x << ' ' << y << ' ' << result);
     return result;
 }
+
+int get(Margin m) {
+    return sum(m.right) - sum(m.left - 1);
+}
+
+struct Request {
+    int x;
+    int index;
+    bool enter;
+
+    friend bool operator<(const Request& a, const Request& b) {
+        if (a.x == b.x) {
+            return a.enter;
+        }
+        return a.x < b.x;
+    }
+};
 
 int main() {
 #ifdef DEBUG
@@ -38,22 +63,40 @@ int main() {
 #endif
     in.sync_with_stdio(false);
     out.sync_with_stdio(false);
-    int n, m;
+    int m;
     in >> n >> m;
-    tree = new char[10000][10000]();
+    std::vector<Request> requests;
+    std::vector<Margin> margins;
+    requests.reserve(2*m);
+    margins.reserve(m);
+    std::vector<int> answers(m);
     for (int c = 0; c < n; ++c) {
-        int x;
-        in >> x;
-        --x;
-        for (int i = c; i < n; i = (i | (i + 1))) {
-            for (int j = x; j < n; j = (j | (j + 1))) {
-                 tree[i][j]++;
-            }
-        }
+        in >> numbers[c];
+        numbers[c]--;
     }
     for (int i = 0; i < m; ++i) {
         int x1, x2, y1, y2;
         in >> x1 >> x2 >> y1 >> y2;
-        out << sum(x2 - 1, y2 - 1) - sum(x1 - 2, y2 - 1) - sum(x2 - 1, y1 - 2) + sum(x1 - 2, y1 - 2) << std::endl;
+        margins.push_back(Margin{y1 - 1, y2 - 1});
+        requests.push_back(Request{x1 - 1, i, true});
+        requests.push_back(Request{x2 - 1, i, false});
+    }
+    std::sort(requests.begin(), requests.end());
+    int cur = 0;
+    for (int c = 0; c < n; ++c) {
+        while (cur < 2*m && requests[cur].x == c && requests[cur].enter) {
+            int index = requests[cur].index;
+            answers[index] = get(margins[index]);
+            ++cur;
+        }
+        add(numbers[c], 1);
+        while (cur < 2*m && requests[cur].x == c) {
+            int index = requests[cur].index;
+            answers[index] = get(margins[index]) - answers[index];
+            ++cur;
+        }
+    }
+    for (int ans : answers) {
+        out << ans << std::endl;
     }
 }
